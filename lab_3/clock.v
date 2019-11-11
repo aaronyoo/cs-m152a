2f03e58b8ad6dbd9ce1cdbd288ffd7da67ffb80a
Furn Techalertumpai <furncyn@ucla.edu>
2019-11-11 14:53:55 -0800

    Clean up code

--- lab_3/clock.v
+++ lab_3/clock.v
@@ -22,42 +22,41 @@ always@ (posedge clk)
             twohz_idx = 0;
             fast_idx = 0;
             blink_idx = 0;
-	end
-	else begin
-            onehz_idx = onehz_idx + 1;
-            twohz_idx = twohz_idx + 1;
-            fast_idx = fast_idx + 1;
-            blink_idx = blink_idx + 1;
+		end
+		else begin
+				onehz_idx = onehz_idx + 1;
+				twohz_idx = twohz_idx + 1;
+				fast_idx = fast_idx + 1;
+				blink_idx = blink_idx + 1;
 
-	    // One hertz clock
-	    // Too slow for simulator
-            // if (onehz_idx == 50000000) begin
-	    if (onehz_idx == 600000) begin
-		$display("IDX: %d, OneCLK: %d", onehz_idx, onehz_clk);
-		onehz_idx = 0;
-		onehz_clk = ~onehz_clk;
-            end
+			// One hertz clock
+			// if (onehz_idx == 50000000) begin // Too slow for simulator
+			if (onehz_idx == 600000) begin
+				$display("IDX: %d, OneCLK: %d", onehz_idx, onehz_clk);
+				onehz_idx = 0;
+				onehz_clk = ~onehz_clk;
+			end
 
-	    // Two hertz clock
-	    if (twohz_idx == 1200000) begin
-		$display("IDX: %d, TwoCLK: %d", twohz_idx, twohz_clk);
-		twohz_idx = 0;
-		twohz_clk = ~twohz_clk;
-            end
+			// Two hertz clock
+			if (twohz_idx == 1200000) begin
+				$display("IDX: %d, TwoCLK: %d", twohz_idx, twohz_clk);
+				twohz_idx = 0;
+				twohz_clk = ~twohz_clk;
+			end
 
-	    // Fast clock
-	    if (fast_idx == 600000) begin
-		// $display("IDX: %d, FastCLK: %d", fast_idx, fast_clk);
-		fast_idx = 0;
-		fast_clk = ~fast_clk;
-            end
+			// Fast clock
+			if (fast_idx == 600000) begin
+				// $display("IDX: %d, FastCLK: %d", fast_idx, fast_clk);
+				fast_idx = 0;
+				fast_clk = ~fast_clk;
+			end
 
-	    // Blink Clock
-	    if (blink_idx == 600000) begin
-		// $display("IDX: %d, SlowCLK: %d", blink_idx, blink_clk);
-		blink_idx = 0;
-		blink_clk = ~blink_clk;
-            end
-	end
+			// Blink Clock
+			if (blink_idx == 600000) begin
+				// $display("IDX: %d, SlowCLK: %d", blink_idx, blink_clk);
+				blink_idx = 0;
+				blink_clk = ~blink_clk;
+			end
+		end
     end
 endmodule
--- lab_3/counter.v
+++ lab_3/counter.v
@@ -3,11 +3,10 @@ module counter(
     output reg [3:0] counter_output
 );
 
-    always@ (posedge clk)
-        begin 
+    always@ (posedge clk) begin 
         if( rst | counter_output == 4'b1001)
             counter_output <= 4'b0000;
         else
             counter_output <= counter_output + 1;
-        end
+    end
 endmodule
